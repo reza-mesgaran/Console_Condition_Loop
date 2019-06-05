9aa9b940b210971f061598d0af2d66dfcce2ad3a
Reza Mesgaran <reza_mesgaran@live.com>
2019-06-05 19:04:33 -0400

    Add project files.

---
+++ Console_Condition_Loop/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Condition_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //------------ Dead Loop
+            while(true)
+            {
+                Console.WriteLine("Dead Loop");
+            }
+            //-----------------------------------------------
+        }
+    }
+}
