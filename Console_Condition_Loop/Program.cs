b748b923a0deaabcff130be2988d3f5742c3350d
Reza Mesgaran <reza_mesgaran@live.com>
2019-06-05 20:00:56 -0400

    The conditional operator :?

--- Console_Condition_Loop/Program.cs
+++ Console_Condition_Loop/Program.cs
@@ -42,29 +42,34 @@ namespace Console_Condition_Loop
             //    y = y + 1;
             //}
             //------------ Break (Jump-Out the Loops)
-            int x = 0;
-            while (x<10)
-            {
-                if (x==5)
-                {
-                    break;  // When x=5 => Exit from While-Loop 
-                }                   
-                Console.WriteLine(x);
-                x++;
-            }
-            //------------ Continue (Just skips the remaining statements of that iteration of the loop.)
-            Console.WriteLine("-------------------------------");
-            int y = 0;
-            while (y<10)
-            {
-                y++;
-                if (y == 5)
-                {
-                    continue;  // When Y=5 => leave current iteration and Go for next iteration
-                }                  
-                Console.WriteLine(y);                
-            }
+            //int x = 0;
+            //while (x<10)
+            //{
+            //    if (x==5)
+            //    {
+            //        break;  // When x=5 => Exit from While-Loop 
+            //    }                   
+            //    Console.WriteLine(x);
+            //    x++;
+            //}
+            ////------------ Continue (Just skips the remaining statements of that iteration of the loop.)
+            //Console.WriteLine("-------------------------------");
+            //int y = 0;
+            //while (y<10)
+            //{
+            //    y++;
+            //    if (y == 5)
+            //    {
+            //        continue;  // When Y=5 => leave current iteration and Go for next iteration
+            //    }                  
+            //    Console.WriteLine(y);                
+            //}
+            //------------ The conditional operator :?
+            int x = 10;
+            string Message = (x > 5) ? "Bigger" : "Smaller";
+            Console.WriteLine(Message);
             Console.ReadKey();
+            //------------
         }
     }
 }
